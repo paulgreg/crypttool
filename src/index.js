7571228f0a1fb513da9a7c0d608631d1d0345191
Grégory Paul <me@gpaul.me>
2022-09-30 20:17:37 +0200

    First version

---
+++ src/index.js
@@ -0,0 +1,98 @@
+const textareaEl = document.querySelector('textarea')
+const formEl = document.querySelector('form')
+const submitEl = document.querySelector('input[type=submit]')
+const passwordEl = document.querySelector('input[type=password]')
+const errorEl = document.querySelector('.error')
+
+const sep = '|=|'
+
+const getMsg = (textareaValue) => {
+  const splittedValue = textareaValue.split(sep)
+
+  if (splittedValue.length === 3) {
+    const [salt, iv, encryptedMsg] = splittedValue
+    return { salt, iv, encryptedMsg }
+  } else {
+    return { clearMsg: textareaValue }
+  }
+}
+
+const switchButton = (encrypted) => {
+  submitEl.value = encrypted ? 'decrypt' : 'encrypt'
+}
+
+let errorTimeout
+
+const displayError = () => {
+  errorEl.style.display = 'block'
+  clearTimeout(errorTimeout)
+  errorTimeout = setTimeout(() => {
+    errorEl.style.display = ''
+  }, 4000)
+}
+
+const encryptOrDecrypt = () =>
+  Promise.resolve()
+    .then(() => {
+      const passwordValue = passwordEl.value
+      const msg = getMsg(textareaEl.value)
+
+      if (msg.salt && msg.iv && msg.encryptedMsg) {
+        return decrypt(passwordValue, msg.salt, msg.iv, msg.encryptedMsg).then(
+          (msg) => {
+            textareaEl.value = msg
+            switchButton(false)
+          }
+        )
+      } else {
+        return encrypt(passwordValue, msg.clearMsg).then((encryptedMsg) => {
+          const { content, salt, iv } = encryptedMsg
+          textareaEl.value = salt + sep + iv + sep + content
+          switchButton(true)
+        })
+      }
+    })
+    .catch((e) => {
+      console.error(e)
+      displayError()
+    })
+
+formEl.addEventListener(
+  'submit',
+  (e) => {
+    e.stopPropagation()
+    e.preventDefault()
+    encryptOrDecrypt()
+  },
+  false
+)
+
+textareaEl.addEventListener(
+  'keydown',
+  (e) => {
+    if (e.ctrlKey && e.key === 'Enter') {
+      e.preventDefault()
+      e.stopPropagation()
+      encryptOrDecrypt()
+    }
+  },
+  false
+)
+
+textareaEl.addEventListener(
+  'keyup',
+  () => {
+    if (textareaEl.value === '') switchButton(false)
+  },
+  false
+)
+
+textareaEl.addEventListener(
+  'paste',
+  (e) => {
+    const pastedText = (e.clipboardData || window.clipboardData).getData('text')
+    const msg = getMsg(pastedText)
+    switchButton(msg.encryptedMsg)
+  },
+  false
+)
