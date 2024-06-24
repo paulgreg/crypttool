e616a2c3ab67a1b0d8b4cdc8140122c3608abd4f
Grégory Paul <me@gpaul.me>
2024-06-24 18:02:38 +0200

    fix: unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,9 @@
 const { TextEncoder, TextDecoder } = require('util')
-const crypto = require('crypto')
+const { webcrypto } = require('crypto')
 
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
 
 Object.defineProperty(global.self, 'crypto', {
-  value: crypto.webcrypto,
+  value: webcrypto,
 })
